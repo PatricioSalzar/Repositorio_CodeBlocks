8d229be8d3230e9427f3821fc2694c72ea80d905
Agustin <agustingonzalez@gmail.com>
2025-05-01 18:21:55 -0300

    nuevo cambio breeooooo

--- main.cpp
+++ main.cpp
@@ -7,5 +7,9 @@ int main()
     cout << "HOLA MI GENTEEEE" << endl;
     cout << "UN NUEVO MENSAJE" << endl;
     cout << "CHUPALA DANTEEEE, A PEDIDO DE AGU" << endl;
+    cout<< "pato toca nenasss"<<endl;
+        cout<< "pato toca nenasss"<<endl;
+
+
     return 0;
 }
