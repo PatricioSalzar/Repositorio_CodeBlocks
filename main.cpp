5cf0bca01d483ec66e2eef168b81450d929c67ac
ddancore <dante.corengia@gmail.com>
2025-05-01 18:28:23 -0300

    Pato Trolo

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int main()
 {
+
     cout << "HOLA MI GENTEEEE" << endl;
     cout << "UN NUEVO MENSAJE" << endl;
     cout << "CHUPALA DANTEEEE, A PEDIDO DE AGU" << endl;
     cout<< "pato toca nenasss"<<endl;
-        cout<< "pato toca nenasss"<<endl;
+    cout<< "pato toca nenasss"<<endl;
+    cout<< "Agus catador de bananas con la cola"<<endl;
+
+
 
 
     return 0;
